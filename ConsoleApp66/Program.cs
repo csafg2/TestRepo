ab81dc5fee51e70a493e319adff02c31c6fc4862
ads3@afgdeveloper.com <ads3@afgdeveloper.com>
2018-05-13 10:11:48 +0430

    simple tessimple test
    simple test
    
    simple test
    
    simple test
    
    simple test

--- ConsoleApp66/Program.cs
+++ ConsoleApp66/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp66
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello you World!");
         }
     }
 }
